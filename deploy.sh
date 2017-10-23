02c1df68358bcdd29be5b3d7afb862536bde4f96
Henryk Konsek <hekonsek@gmail.com>
2017-10-23 09:06:15 +0200

    Added Ansible script.

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --net=host -e "xpack.security.enabled=false" -e "discovery.type=single-node" -v cloud-metrics-elasticsearch:/usr/share/elasticsearch/data  docker.elastic.co/elasticsearch/elasticsearch:5.6.3
-docker run -d --net=host grafana/grafana

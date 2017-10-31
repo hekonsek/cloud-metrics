4c5bed548c6789e774942e7e58b88e56d0e23199
Henryk Konsek <hekonsek@gmail.com>
2017-10-31 09:19:36 +0100

    Fixed Grafana templates.

--- server/src/main/groovy/cloudmetrics/server/grafana/GrafanaTemplates.groovy
+++ server/src/main/groovy/cloudmetrics/server/grafana/GrafanaTemplates.groovy
@@ -4,7 +4,7 @@ import cloudmetrics.server.metrics.Metric
 import org.apache.commons.io.IOUtils
 
 import static com.google.common.base.Charsets.UTF_8
-//import static io.vertx.core.json.Json.decodeValue
+import static json4dummies.Json.fromJson
 
 class GrafanaTemplates {
 
@@ -12,11 +12,11 @@ class GrafanaTemplates {
         def template = IOUtils.toString(getClass().getResourceAsStream('/dashboard-row.json'), UTF_8)
         template = template.replaceAll(/METRIC/, metric.key)
         template = template.replaceAll(/TYPE/, metric.type())
-        decodeValue(template.replaceAll(/NODE/, metric.key.replaceFirst(/node\./, '').replaceFirst(/\..+/, '')), Map)
+        fromJson(template.replaceAll(/NODE/, metric.key.replaceFirst(/node\./, '').replaceFirst(/\..+/, '')), Map)
     }
 
     static Map<String, Object> emptyDashboard() {
-        decodeValue(IOUtils.toString(getClass().getResourceAsStream('/dashboard-empty.json'), UTF_8), Map)
+        fromJson(IOUtils.toString(getClass().getResourceAsStream('/dashboard-empty.json'), UTF_8), Map)
     }
 
     static String target(String metric, int refid) {
